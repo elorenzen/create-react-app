6a97a8e1b37b93551eee0979579266b2f57ac407
elorenzen <eric.lorenzen@gmail.com>
2019-05-22 10:17:07 -0600

    Imported React and ReactDOM into index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import React and the ReactDOM libraries
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a React component
 
 // Take React component and show it on screen
