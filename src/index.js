e6baa1f5853a7e3b279932337ac31b10ae72baaa
elorenzen <eric.lorenzen@gmail.com>
2019-05-22 10:22:44 -0600

    Created a React component and rendered it on screen

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Create a React component
-
+const App = () => {
+    return <div>Hi there</div>
+};
 // Take React component and show it on screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
