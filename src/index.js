d5b93f643714fbdcd294d7d5989e68c53ae978a6
elorenzen <eric.lorenzen@gmail.com>
2019-05-22 12:28:33 -0600

    changed label class attribute to className, according to JSX syntax

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
     return(
         <div>
-	        <label class="label" for="name">Enter name:</label>
+	        <label className="label" for="name">Enter name:</label>
 	        <input type="text" id="name" />
 	        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
         </div>
