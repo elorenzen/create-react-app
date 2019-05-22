cf733c0926a8418db53963da82399f39fabf8d58
elorenzen <eric.lorenzen@gmail.com>
2019-05-22 12:46:23 -0600

    Created function and passed it into button JSX

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // Import React and the ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function getButtonText(){
+    return "CLICK ME";
+}
+
 // Create a React component
 const App = () => {
     return(
         <div>
 	        <label className="label" for="name">Enter name:</label>
 	        <input type="text" id="name" />
-	        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+	        <button style={{marginLeft: '4px', backgroundColor: 'blue', color: 'white'}}>
+                {getButtonText()}
+            </button>
         </div>
     )
 };
