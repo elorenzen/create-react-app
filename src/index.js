e39861368d081d32b8e9546ca1a401a620d0ecb2
elorenzen <eric.lorenzen@gmail.com>
2019-05-22 12:22:45 -0600

    Created basic label, input, and button using JSX

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Create a React component
 const App = () => {
-    return <div>Hi there</div>
+    return(
+        <div>
+	        <label class="label" for="name">Enter name:</label>
+	        <input type="text" id="name" />
+	        <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    )
 };
 // Take React component and show it on screen
 ReactDOM.render(
